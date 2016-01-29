116f20b0a3479f3ae2dd6c7cc4de0a1368229ba3
Shea Munion <sheamunion@gmail.com>
2016-01-29 10:05:27 -0600

    Remove superfluous comments from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 user = User.create(
        :first_name => "Aleksandra",
        :last_name => "Nowak",
@@ -26,4 +17,3 @@ user = User.create(
 
  User.skills.create(name: "Spanish")
 
- 
