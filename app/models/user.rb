3c0186a5f5b5d6322e622a575811b5dcd6075899
Shea Munion <sheamunion@gmail.com>
2016-01-29 14:36:28 -0600

    Add User#full_name and User#city_state helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ActiveRecord::Base
 
 	has_many :sent_requests, class_name: "Request", foreign_key: :sender_id #w
 	has_many :recieved_requests, class_name: "Request", foreign_key: :reciever_id #w
-	has_many :collections, foreign_key: :teacher_id #w 
+	has_many :collections, foreign_key: :teacher_id #w
 	has_many :skills, through: :collections #w
 	has_many :messages #w
+
+
+	def full_name
+		"#{self.first_name} #{self.last_name}"
+	end
+
+	def city_state
+		"#{self.city}, #{self.state}"
+	end
 end
