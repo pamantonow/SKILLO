257cfd4dbcd13c4cdc07bacb17df293ed85e5d5e
pamantonow <unstyleurbain@gmail.com>
2016-01-29 18:52:17 -0600

    Fix merge conflicts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,7 +8,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-user = User.create!(
+
+user = User.create(
+
        :first_name => "Aleksandra",
        :last_name => "Nowak",
        :email => "anowak@icstars.org",
@@ -24,6 +27,8 @@ user = User.create!(
        :avatar => "https://scontent.ford1-1.fna.fbcdn.net/hphotos-xfa1/v/t1.0-9/12193604_985555058171660_8392627846667778134_n.jpg?oh=fb6abe3148f2e200440885280f135e22&oe=5730A300"
    )
 
+
 user.skills.create(name: "Spanish") 
 
  Request.create(sender_id: 1, reciever_id: 2 , skill_id: 1, accepted: true , content: "I will like to tutor with you, if you are available?")
+
