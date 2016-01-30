37b3483029478774a994f22a34b2159c4caed9ee
Shea Munion <sheamunion@gmail.com>
2016-01-30 16:03:04 -0600

    Seeded users now have valid, random addresses for testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,85 @@
+require 'nokogiri'
+require 'open-uri'
+
+class FakeName
+  ENDPOINT = 'https://fakena.me/random-real-address/'
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(ENDPOINT))
+  end
+end
+
+#=====================begin==============================
+# CREATE USERS
+# Create education options for users
+education_options = ["High-School","Bachelors Degree","Masters Degree"]
+
+# Create 3 users
+3.times do
+	first_name = Faker::Name.first_name
+	last_name = Faker::Name.last_name
+	st_num_name = FakeName.new.doc.at_xpath("//strong").children[0].text
+	city_state_zip = FakeName.new.doc.at_xpath("//strong").children[2].text
+	User.create!(
+		:first_name => first_name,
+		:last_name => last_name,
+		:email => Faker::Internet.email(first_name + "." + last_name),
+		:password => "password",
+		:city => city_state_zip.split(',').first,
+		:state => city_state_zip.match(/\b(\w{2})\b/),
+		:zip => city_state_zip.match(/\b(\d{5})/).to_s.to_i,
+		:st_num => st_num_name.match(/^(\d+\W|[a-z]+)?(\d+)([a-z]?)\b/).to_s.to_i,
+		:st_name => st_num_name.split(' ').drop(1).join(' '),
+		:phone => Faker::PhoneNumber.cell_phone,
+		:occupation => Faker::Name.title,
+		:education => education_options.shuffle.last,
+		:description => Faker::Lorem.paragraph(sentence_count = 10),
+		:avatar => Faker::Avatar.image
+	)
+end
+#^^^^^^^^^^^^^^^^^end^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+#=====================begin==============================
+# CREATE CATEGORIES AND SKILLS
 # Create 11 categories
 categories = ["Art","Business","Communication","Cooking","IT","Languages","Manufacturing","Music","Science","Sport","Others"]
 categories.each do |category|
  Category.create!(name: category)
 end
 
-# Create 3 users
-3.times do
-	User.create!(
-		:first_name => "Ana",
-		:last_name => "Nowak",
-		:email => "qwff@icstars.org",
-		:password => "olaolaola",
-		:city => "Chicago",
-		:state => "IL",
-		:zip => 60654,
-		:st_num => 900,
-		:st_name => "Michigan Ave",
-		:phone => "7736009632",
-		:occupation => "Developer",
-		:education => "High-School",
-		:description => "Intelligentsia blog fap waistcoat, biodiesel viral disrupt. Next level schlitz vinyl, thundercats shabby chic freegan roof party affogato plaid. Farm-to-table readymade bitters, literally lo-fi drinking vinegar raw denim marfa pour-over shabby chic umami hella bushwick PBR&B austin. Single-origin coffee microdosing shabby chic, fingerstache meditation listicle freegan retro affogato church-key. ",
-		:avatar => "https://scontent.ford1-1.fna.fbcdn.net/hphotos-xfa1/v/t1.0-9/12193604_985555058171660_8392627846667778134_n.jpg?oh=fb6abe3148f2e200440885280f135e22&oe=5730A300"
-	)
+# Create three skills for the IT category
+programming_languages =["Ruby", "PHP", "JavaScript"]
+
+programming_languages.each do |skill|
+      Category.where(name: "IT").first.skills.create!(name: skill)
 end
 
-Skill.create(name: "Spanish")
-user.collections.create(skill_id: 1, years_of_experience: 2, hourly_rate: 13, description: "I am really good at teaching and have an intermediate fluency with Spanish.")
+# Create three skills for the language category
+languages =["Spanish", "English", "Polish"]
+
+languages.each do |skill|
+      Category.where(name: "Languages").first.skills.create!(name: skill)
+end
+
+# Create three skills for the music category
+music_skills =["Guitar", "Music theory", "Piano"]
+
+music_skills.each do |skill|
+      Category.where(name: "Music").first.skills.create!(name: skill)
+end
+#^^^^^^^^^^^^^^^^^end^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
 
-# users = User.all
-# skillsset =["Ruby", "C#", "CSS", "JS", "Python", "Java", "SQL"]
+# user.collections.create(skill_id: 1, years_of_experience: 2, hourly_rate: 13, description: "I am really good at teaching and have an intermediate fluency with Spanish.")
 
-# users.each do |user|
-#       user.skills.build(name: skillsset[rand(0..skillsset.length)])
-#       user.save
-# end
 
 # Request.create(sender_id: 1, reciever_id: 2 , skill_id: 1, accepted: true , content: "I will like to tutor with you, if you are available?")
 
 # skills.each do |skill|
 #       Skill.create!(name: skill, category_id: 1)
 # end
-languages =["Spanish", "English", "Polish"]
 
-languages.each do |skill|
-      Skill.create!(name: skill, category_id: 2)
-end
 
 # users.each do |user|
 #       user.skills.build(name: languages[rand(0..languages.length)])
