4593b4f461a2b2950f6d4b5908da83408a88ddf1
nowakoff <anowak@icstars.org>
2016-01-30 05:47:09 -0600

    Add categories associations with the user

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,32 @@
 class CategoriesController < ApplicationController
-		def show
+		def index
 		@category = Category.find(params[:id])
 		@category.skills.each do |skill|
 			@teachers << skill.users
 		end
-		@users.flatten
+		@teacher_by_category.flatten
 	end
+
+		def show
+		@category = Category.find(params[:id])
+		@categories = Category.all
+		@category_teachers = @category.teachers 
+			@teacher_by_category = @category_teachers.near(current_user.city, 50)
+			@hash = Gmaps4rails.build_markers(@category_teachers) do |user, marker|
+				if user.id == current_user.id
+					marker.lat user.latitude
+					marker.lng user.longitude
+					marker.picture({
+		       "url" => "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
+		       "width" =>  32,
+		       "height" => 32})
+					marker.json({:first_name => user.first_name })
+					marker.infowindow render_to_string(:partial => "/users/tag", :locals => { :user => user})
+				else
+					marker.lat user.latitude
+					marker.lng user.longitude
+					marker.infowindow render_to_string(:partial => "/users/tag", :locals => { :user => user})
+				end
+			end
+		end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,30 +9,57 @@
 
 
 
-user = User.create!(
+# user = User.create!(
 
-       :first_name => "Ana",
-       :last_name => "Nowak",
-       :email => "qwff@icstars.org",
-       :password => "olaolaola",
-       :city => "Chicago",
-       :state => "IL",
-       :zip => "60654",
-       :st_num => "900",
-       :st_name => "Michigan Ave",
-       :phone => "7736009632",
-       :occupation => "Developer",
-       :education => "High-School",
-       :description => "Intelligentsia blog fap waistcoat, biodiesel viral disrupt. Next level schlitz vinyl, thundercats shabby chic freegan roof party affogato plaid. Farm-to-table readymade bitters, literally lo-fi drinking vinegar raw denim marfa pour-over shabby chic umami hella bushwick PBR&B austin. Single-origin coffee microdosing shabby chic, fingerstache meditation listicle freegan retro affogato church-key. ",
-       :avatar => "https://scontent.ford1-1.fna.fbcdn.net/hphotos-xfa1/v/t1.0-9/12193604_985555058171660_8392627846667778134_n.jpg?oh=fb6abe3148f2e200440885280f135e22&oe=5730A300"
-   )
+#        :first_name => "Ana",
+#        :last_name => "Nowak",
+#        :email => "qwff@icstars.org",
+#        :password => "olaolaola",
+#        :city => "Chicago",
+#        :state => "IL",
+#        :zip => "60654",
+#        :st_num => "900",
+#        :st_name => "Michigan Ave",
+#        :phone => "7736009632",
+#        :occupation => "Developer",
+#        :education => "High-School",
+#        :description => "Intelligentsia blog fap waistcoat, biodiesel viral disrupt. Next level schlitz vinyl, thundercats shabby chic freegan roof party affogato plaid. Farm-to-table readymade bitters, literally lo-fi drinking vinegar raw denim marfa pour-over shabby chic umami hella bushwick PBR&B austin. Single-origin coffee microdosing shabby chic, fingerstache meditation listicle freegan retro affogato church-key. ",
+#        :avatar => "https://scontent.ford1-1.fna.fbcdn.net/hphotos-xfa1/v/t1.0-9/12193604_985555058171660_8392627846667778134_n.jpg?oh=fb6abe3148f2e200440885280f135e22&oe=5730A300"
+#    )
 
 
-categories = ["IT", "Languages", "Cooking", "Sport", "Art","Manufacturing" ,"Communication" ,"Music","Science","Business" ,"Others"]
-categories.each do |category|
- Category.create(name: category)     
+# categories = ["IT", "Languages", "Cooking", "Sport", "Art","Manufacturing" ,"Communication" ,"Music","Science","Business" ,"Others"]
+# categories.each do |category|
+#  Category.create(name: category)     
+# end
+
+# users = User.all
+# skillsset =["Ruby", "C#", "CSS", "JS", "Python", "Java", "SQL"]
+
+# skills.each do |skill|
+#       Skill.create!(name: skill, category_id: 1)
+# end
+
+# users.each do |user|
+#       user.skills.build(name: skillsset[rand(0..skillsset.length)])
+#       user.save
+# end
+
+# Request.create(sender_id: 1, reciever_id: 2 , skill_id: 1, accepted: true , content: "I will like to tutor with you, if you are available?")
+
+
+
+
+# skills.each do |skill|
+#       Skill.create!(name: skill, category_id: 1)
+# end
+languages =["Spanish", "English", "Polish"]
+
+languages.each do |skill|
+      Skill.create!(name: skill, category_id: 2)
 end
 
-
-Request.create(sender_id: 1, reciever_id: 2 , skill_id: 1, accepted: true , content: "I will like to tutor with you, if you are available?")
-
+# users.each do |user|
+#       user.skills.build(name: languages[rand(0..languages.length)])
+#       user.save
+# end
