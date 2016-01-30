0bb57ce231fe2b90ede8d80a209e1fe0d8d7ba24
pamantonow <unstyleurbain@gmail.com>
2016-01-30 10:57:19 -0600

    Fix login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,40 +7,17 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		p params
 		@user = User.find_by(email: session_params[:email])
-		p @user
     if @user && @user.authenticate(session_params[:password])
 	    session[:user_id] = @user.id
-	    p session[:user_id]
 	    redirect_to user_path(@user)
     else
-    	p @user
-    	puts "-----------"
-    	p "Credentials don't match"
-
-    	puts "++++++++++"
+    	flash[:notice] = "Yoour Credentials don't match"
       render :'sessions/new'
     	end
     end
 
-		# @user = User.authenticate(params[:email], params[:password])
-		# if @user
-		# puts "----------"
-		# 	p @user
-		# 	 session[:user_id] = @user.id
-		# 	puts "++++++++++++"
-			
-
-		# 	 redirect_to user_path
-	# 	else
-	# 		p @user
-	# 		p "Credentials don't match"
-	# 		flash[:notice] = "Credentials don't match"
-	# 		render :'sessions/new'
-	# 	end
-	# end
-
+	
 	def destroy
 		session[:user_id] = nil
 		redirect_to '/login'
