cd7b93a37127d17beaf0090c769e44455bb6486b
Shea Munion <sheamunion@gmail.com>
2016-02-03 14:27:29 -0600

    Rename authorized? to current_user?

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,22 +7,38 @@ class CollectionsController < ApplicationController
 
 	def edit
 		@collection = Collection.find(params[:id])
+		if !current_user?(@collection.teacher)
+			redirect_to current_user
+		end
 	end
 
 	def update
 		@collection = Collection.find(params[:id])
-		@collection.assign_attributes(params_collection)
-		@collection.assign_attributes(years_of_experience: @collection.years_of_experience.to_i)
-		@collection.assign_attributes(hourly_rate: @collection.hourly_rate.to_i)
-		if @collection.save
-      redirect_to current_user
-    else
-    	render 'edit'
+		if current_user?(@collection.teacher)
+			@collection.assign_attributes(params_collection)
+			@collection.assign_attributes(years_of_experience: @collection.years_of_experience.to_i)
+			@collection.assign_attributes(hourly_rate: @collection.hourly_rate.to_i)
+			if @collection.save
+	      redirect_to current_user
+	    else
+	    	render 'edit'
+			end
+		else
+			redirect_to current_user
 		end
 	end
 
-	def delete
-
+	def destroy
+		@collection = Collection.find(params[:id])
+		if current_user?(@collection.teacher)
+			if @collection.destroy
+				redirect_to current_user
+			else
+				redirect_to :back
+			end
+		else
+			redirect_to current_user
+		end
 	end
 
 	private
